7ec1153f2d879693bf7a4ccda9b1232503a5fab2
BipradipBhowmik2006 <its16bipradip@gmail.com>
2026-08-16 19:20:48 +0530

    Add a C program to check if a year is a leap year
    - Implemented a simple console application that prompts the user to enter a year.
    - The program checks if the entered year is a leap year based on the standard rules.
    - Includes necessary header files and basic input/output functionality.

---
+++ Bipradip Bhowmik/C/4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    print("Enter the first number");
+    scanf("%d",&a);
+    printf("Enter the second number");
+    scanf("%d",&b);
+    printf("multiplication=%d",a*b);
+    return 0;
+}
