b417a9962a58a3d328a23a58728acc084ee4375a
Utpal-Sen-Gupta <usengupta737@gmail.com>
2026-08-20 00:19:48 +0530

    Merge

---
+++ Utpal Sen Gupta/Practice/Marks v/s grade.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter the respective marks:";
+    cin>>m;
+    if( m>=90){
+        cout<<"Grade A";
+    }
+    else if(m>=80){
+        cout<<"Grade B";
+    }
+     else if(m>=70){
+        cout<<"Grade C";
+     }
+      else if(m>=60){
+        cout<<"Grade D";
+      }
+       else if(m>=50){
+        cout<<"Grade E";
+       }
+        else if(m>=40){
+        cout<<"Aap fail ho gaye!!!";
+        }
+     return 0;   
+}
