d42118a9af18ebb1e51afebd0c1cbfa27fc48a77
RachanaGhosh <rachanasana18@gmail.com>
2026-08-28 23:57:31 +0530

    new

---
+++ Rachana_Ghosh/Python/calculator.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+sum=a+b
+difference=a-b
+product=a*b
+quotient=a/b
+print(f"Sum:{sum}")
+print(f"difference:{difference}")
+print(f"Product:{product}")
+print(f"Quotient:{quotient}")
