c92eb12a9db8646a5ba75a6543bf5ec0116bbc85
Utpal-Sen-Gupta <usengupta737@gmail.com>
2026-08-30 01:39:58 +0530

    merge

---
+++ Utpal Sen Gupta/Practice/SI.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float p,r,t;
+    cout<<"Enter p:";
+    cin>>p;
+    cout<<"Enter r:";
+    cin>>r;
+    cout<<"Enter t:";
+    cin>>t;
+    cout<<"Interest:"<<(p*r*t)/100<<endl;
+    return 0;
+}
